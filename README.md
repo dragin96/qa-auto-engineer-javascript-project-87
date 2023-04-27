6041a4431b4a8758a6a225a90859743501e81a08
alexeyivanov <aivanov@primexbt.ch>
2023-04-27 09:57:52 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dragin96/qa-auto-engineer-javascript-project-87/workflows/hexlet-check/badge.svg)](https://github.com/dragin96/qa-auto-engineer-javascript-project-87/actions)
+
+### Setup
+
+```sh
+$ npm install
+$ node bin/genDiff.js filepath1 filepath2
+$ npm publish --dry-run
+```
+[![asciicast](https://asciinema.org/a/URR2MtpoMViMnxxpeSnsMxZXk.svg)](https://asciinema.org/a/URR2MtpoMViMnxxpeSnsMxZXk)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,6 @@
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
 const program = new Command();
 
 program
@@ -9,5 +11,31 @@ program
 program
     .arguments('<filepath1> <filepath2>')
     .option('--format <type>', 'output format')
+    .action((filepath1, filepath2, options ) => {
+        const optionsFormat = options.format || 'json';
+        const fullPathFile1 = path.resolve(process.cwd(), filepath1);
+        const fullPathFile2 = path.resolve(process.cwd(), filepath2);
+        const jsonFile1 = fs.readFileSync(fullPathFile1, 'utf-8');
+        const jsonFile2 = fs.readFileSync(fullPathFile2, 'utf-8');
+        const obj1 = JSON.parse(jsonFile1);
+        const obj2 = JSON.parse(jsonFile2);
+        const keys1 = Object.keys(obj1);
+        const keys2 = Object.keys(obj2);
+        const allKeys = new Set([...keys1, ...keys2]);
+        const diff = Array.from(allKeys).sort().reduce((acc, key) => {
+            if (keys1.includes(key) && keys2.includes(key)) {
+                if (obj1[key] === obj2[key]) {
+                    return acc + `\n    ${key}: ${obj1[key]}`;
+                } else {
+                    return acc + `\n  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+                }
+            } else if (keys1.includes(key)) {
+                return acc + `\n  - ${key}: ${obj1[key]}`;
+            } else {
+                return acc + `\n  + ${key}: ${obj2[key]}`;
+            }
+        }, '{') + '\n}';
+        console.log(diff);
+    });
 
 program.parse();
