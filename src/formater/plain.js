8e2ed0608736603990234f46f064cd2ca8f517bd
alexeyivanov <aivanov@primexbt.ch>
2023-05-01 00:39:41 +0300

    step 6 fix

--- src/formater/plain.js
+++ src/formater/plain.js
@@ -1,17 +1,17 @@
 export const plain = (tree) => {
-  return tree.reduce((acc, el) => {
+  return tree.map((acc, el) => {
     if (el.type === 'added') {
-      acc += `Property '${el.key}' was added with value: ${el.value}\n`;
+      acc = [...acc, `Property '${el.key}' was added with value: ${el.value}`];
     }
     if (el.type === 'deleted') {
-      acc += `Property '${el.key}' was removed\n`;
+      acc = [...acc, `Property '${el.key}' was removed`];
     }
     if (el.type === 'changed') {
-      acc += `Property '${el.key}' was updated. From ${el.oldValue} to ${el.value}\n`;
+      acc = [...acc, `Property '${el.key}' was updated. From ${el.oldValue} to ${el.value}`];
     }
     if (el.type === 'unchanged') {
-      acc += '';
+      return acc;
     }
     return acc;
-  }, '');
+  }, '').join('\n');
 }
